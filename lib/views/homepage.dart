d89c736d0ac21bb1e9840e82cd21697ceea13d7c
Ahmad Einieh <66053897+ahmad-einieh@users.noreply.github.com>
2023-02-19 10:04:58 +0300

    new temp

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,7 +1,9 @@
 import 'package:encryption_decryption/helper/methods.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../helper/style.dart';
+import '../router/router.dart';
 import 'widgets/homepage_widgets/home_page_content.dart';
 import 'widgets/homepage_widgets/upper_bar.dart';
 
@@ -10,15 +12,27 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: mainDecoration,
-        child: Column(
-          children: [
-            const UpperBar(),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.22),
-            const HomePageContent(),
-          ],
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKey: (event) {
+        var letter = event.logicalKey.keyLabel;
+        if (letter == "R" || letter == "r") {
+          Get.toNamed(RouterName.rsa);
+        } else if (letter == "A" || letter == "a") {
+          Get.toNamed(RouterName.aes);
+        }
+      },
+      child: Scaffold(
+        body: Container(
+          decoration: mainDecoration,
+          child: Column(
+            children: [
+              const UpperBar(),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.22),
+              const HomePageContent(),
+            ],
+          ),
         ),
       ),
     );
